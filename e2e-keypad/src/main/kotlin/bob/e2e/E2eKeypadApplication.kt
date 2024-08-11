789225878b52d6fb7e250566713d38d677ce8c00
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2024-08-11 15:56:10 +0900

    참고링크 추가
    참고할 만한 사이트 링크 추가

--- e2e-keypad/src/main/kotlin/bob/e2e/E2eKeypadApplication.kt
+++ e2e-keypad/src/main/kotlin/bob/e2e/E2eKeypadApplication.kt
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+// 참고 사이트: https://itmoon.tistory.com/124?category=948164
 @SpringBootApplication
 class E2eKeypadApplication {
     @Bean
