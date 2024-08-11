91b5d8147a50be82c2f87fb2c2fcdb4d2dc8a152
양준헌 <97436830+greyhawk16@users.noreply.github.com>
2024-08-11 10:14:01 +0900

    키패드 제작 함수 변경
    get 요청으로 키패드를 생성하도록 변경

--- e2e-keypad/src/main/kotlin/bob/e2e/controller/KeypadController.kt
+++ e2e-keypad/src/main/kotlin/bob/e2e/controller/KeypadController.kt
@@ -21,42 +21,6 @@ import java.util.Base64
 @RestController
 @RequestMapping("/keypad")
 class KeypadController {
-    // 추후: POST 요청 & HMAC에 쓸 KEY 전송 -> 키패드 세션 식별자, 숫자쌍 & 키패드 이미지, HMAC 반환
-    @PostMapping("/create_keypad")
-    fun CreateKeypad(@RequestBody hmacKey: String): ResponseEntity<Map<String, Any>> {
-        val keypadService = KeypadService()
-        val keypadImages = keypadService.createHashImageMap()
-        val keypadNumHashes = keypadService.generateRandomHashes()
-
-        // 해시값-이미지 연결한 Map 생성
-        // 추후 shuffle 적용 필요
-        val hashImageMap = mutableMapOf<String, String>()
-        for ((key, hashValue) in keypadNumHashes) {
-            hashImageMap[hashValue] = keypadImages[key] ?: ""
-        }
-
-        val keypadSessionId = keypadService.generateRandomHash()
-        val keypadHmac = keypadService.generateHMAC(keypadSessionId, hmacKey)
-
-        val responseBody = mapOf(
-            "keypad" to hashImageMap,
-            "HMAC" to keypadHmac,
-            "keypadSessionId" to keypadSessionId
-        )
-
-        val dotenv = Dotenv.load()
-        val dbKey = dotenv["NumToHashMap"]
-        val keypadRepository = KeypadRepository()
-        keypadRepository.storeHashImageMap(keypadNumHashes, dbKey)
-
-
-        // Create the response body
-        return ResponseEntity
-            .status(HttpStatus.OK)
-            .contentType(MediaType.APPLICATION_JSON)
-            .body(responseBody)
-    }
-
     @GetMapping("/keypad_information")
     fun RetrieveKeypad(): ResponseEntity<Map<String, Any>> {
         val keypadRepository = KeypadRepository()
@@ -74,6 +38,23 @@ class KeypadController {
     fun renderKeypad(): ResponseEntity<ByteArray> {
         val keypadService = KeypadService()
         val keypadImages = keypadService.createHashImageMap()
+        val keypadNumHashes = keypadService.generateRandomHashes()
+
+        // 해시값-이미지 연결한 Map 생성
+        // 추후 shuffle 적용 필요
+        val hashImageMap = mutableMapOf<String, String>()
+        for ((key, hashValue) in keypadNumHashes) {
+            hashImageMap[hashValue] = keypadImages[key] ?: ""
+        }
+
+        val hmacKey = keypadService.generateRandomHash()
+        val keypadSessionId = keypadService.generateRandomHash()
+        val keypadHmac = keypadService.generateHMAC(keypadSessionId, hmacKey)
+
+        val dotenv = Dotenv.load()
+        val dbKey = dotenv["NumToHashMap"]
+        val keypadRepository = KeypadRepository()
+        keypadRepository.storeHashImageMap(keypadNumHashes, dbKey)
 
         val combinedImage = combineImages(keypadImages.values.toList().shuffled())
 
